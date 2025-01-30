12441d2b969979fc79e5e5eeb69a02d0f6f40696
ArdillaV21 <fedevalle21@gmail.com>
2025-01-30 11:50:48 -0300

    Verifico que este todo bien desplegado

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className='bg-red-400'>
+      <div className='bg-blue-400'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
